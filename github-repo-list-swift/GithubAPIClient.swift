8388401e75034e7a3da9e91b14cd4a47f6bd6cce
Ian Alexander Rahman <ianrahman@users.noreply.github.com>
2016-07-29 13:46:36 -0400

    Update GithubAPIClient.swift

--- github-repo-list-swift/GithubAPIClient.swift
+++ github-repo-list-swift/GithubAPIClient.swift
@@ -2,7 +2,7 @@
 //  GithubAPIClient.swift
 //  github-repo-list-swift
 //
-//  Created by Haaris Muneer on 6/28/16.
+//  Created by Haaris Muneer and Ian Rahman on 6/28/16.
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
@@ -11,13 +11,12 @@ import UIKit
 class GithubAPIClient {
         
     class func getRepositoriesWithCompletion(completion: [[String:AnyObject]] -> ()) {
-        let urlString = "\(githubAPIURL)/repositories?client_id=\(githubClientID)&client_secret=\(githubClientSecret)"
+        let urlString = "\(Secrets.githubAPIURL)/repositories?client_id=\(Secrets.githubClientID)&client_secret=\(Secrets.githubClientSecret)"
         let url = NSURL(string: urlString)
         let session = NSURLSession.sharedSession()
         guard let potentialURL = url else {assertionFailure("Invalid URL"); return}
         let task = session.dataTaskWithURL(potentialURL) { (data, response, error) in
             guard let data = data else { assertionFailure("Unable to get data \(error?.localizedDescription)"); return }
-            
             if let responseArray = try? NSJSONSerialization.JSONObjectWithData(data, options: []) as? [[String:AnyObject]] {
                 if let responseArray = responseArray {
                     completion(responseArray)
